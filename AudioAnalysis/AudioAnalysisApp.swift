47cc60f5668ddccb914e56fa111b5a608e5aee1d
Abeer Adeel <aadeel@uoguelph.ca>
2021-01-10 00:35:08 -0500

    Initial Commit

---
+++ AudioAnalysis/AudioAnalysisApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAnalysisApp.swift
+//  AudioAnalysis
+//
+//  Created by Abeer Adeel on 2021-01-10.
+//
+
+import SwiftUI
+
+@main
+struct AudioAnalysisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
